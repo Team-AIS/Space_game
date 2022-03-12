48d79a281eb0d6fef1c88f00f4916062b74ed055
Team-AIS <101408506+Team-AIS@users.noreply.github.com>
2022-03-12 17:21:37 +0200

    Update Space_game.py

--- Space_game.py
+++ Space_game.py
@@ -1,128 +1,125 @@
 
 import pygame
-import time
 pygame.init()
 
-ok = True
+x1=50
+wc1=0
+xx1=10
+yy1=10
+y1=10
+yl1=y1
+xl1=x1
+im=True
+a1=True
+b1=False
+def sepiq(xx1,yy1):
+    global wc1
+    if wc1 + 1 >= 27:
+        wc1 = 0
+    if im1:
+        win.blit(im1[wc1//3], (x1,y1))
+        wc1 += 1
+    else:
+        win.blit(im2, (x1, y1))
+        walkCount = 0
+    pygame.display.update()
+im1=[pygame.image.load('sepiq1.png'),pygame.image.load('sepiq2.png'),pygame.image.load('sepiq3.png'),pygame.image.load('sepiq4.png'),pygame.image.load('sepiq3.png'),pygame.image.load('sepiq2.png'),pygame.image.load('sepiq3.png'),pygame.image.load('sepiq4.png'),pygame.image.load('sepiq3.png')]
+im2=pygame.image.load('sepiq1.png')
+win = pygame.display.set_mode((1378, 700))
+right=False
+left=False
+up=False
+down=False
+wc=0
+ok= True
+x=50
+y=50
+width=40
+height=60
+vel=25
+walkRight = [pygame.image.load('4.png'),pygame.image.load('2.png'),pygame.image.load('3.png'),pygame.image.load('4.png'),pygame.image.load('4.png'),pygame.image.load('2.png'),pygame.image.load('3.png'),pygame.image.load('4.png'),pygame.image.load('4.png')]
+walkLeft = [pygame.image.load('44.png'),pygame.image.load('22.png'),pygame.image.load('33.png'),pygame.image.load('44.png'),pygame.image.load('44.png'),pygame.image.load('22.png'),pygame.image.load('33.png'),pygame.image.load('44.png'),pygame.image.load('44.png')]
+Up = [pygame.image.load('43.png'),pygame.image.load('23.png'),pygame.image.load('37.png'),pygame.image.load('43.png'),pygame.image.load('43.png'),pygame.image.load('23.png'),pygame.image.load('37.png'),pygame.image.load('43.png'),pygame.image.load('43.png')]
+Down = [pygame.image.load('45.png'),pygame.image.load('25.png'),pygame.image.load('35.png'),pygame.image.load('45.png'),pygame.image.load('45.png'),pygame.image.load('25.png'),pygame.image.load('35.png'),pygame.image.load('45.png'),pygame.image.load('45.png')]
+
+char = pygame.image.load('1.png')
 clock = pygame.time.Clock()
-win = pygame.display.set_mode((500, 500))
-x=10
-y=10
-gas = 0
-spir = 0
+
+def redrawGameWindow():
+    global wc
+    
+    if wc + 1 >= 27:
+        wc = 0
+    if left:
+        win.blit(walkLeft[wc//3], (x,y))
+        wc += 1 
+    elif right:
+        win.blit(walkRight[wc//3], (x,y))
+        wc += 1
+    elif up:
+        win.blit(Up[wc//3], (x,y))
+        wc += 1
+    elif down:
+        win.blit(Down[wc//3], (x,y))
+        wc += 1
+    else:
+        win.blit(char, (x, y))
+        walkCount = 0
+    pygame.display.update()
 
 while ok:
-    pygame.time.delay(90)
-    clock.tick(27)
+    pygame.time.delay(100)
+    clock.tick(60)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             ok=False
 
-        keys = pygame.key.get_pressed()
+    keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_ESCAPE]:
-            ok = False
-        if keys[pygame.K_d]:
-            if gas == 3:
-                for n in range(1,20):
-                    x=x-2
-                time.sleep(0.1)
-                for n in range(1,10):
-                    x=x-2
-            gas = 1
-        if keys[pygame.K_s]:
-            if gas == 4:
-                for n in range(1,20):
-                    y=y-2
-                time.sleep(0.1)
-                for n in range(1,10):
-                    y=y-2
-            gas = 2 
-        if keys[pygame.K_a]:
-            if gas == 1:
-                for n in range(1,20):
-                    x=x+2
-                time.sleep(0.1)
-                for n in range(1,10):
-                    x=x+2
-            gas = 3 
-        if keys[pygame.K_w]:
-            if gas == 2:
-                for n in range(1,20):
-                    y=y+2
-                time.sleep(0.1)
-                for n in range(1,10):
-                    y=y+2
-            gas = 4 
-        if keys[pygame.K_SPACE]:
-            spir = 1
-    if gas == 1:
-        x=x+10      
-    if gas == 2:
-        y=y+10
-    if gas == 3:
-        x=x-10
-    if gas == 4:
-        y=y-10
-
-
-    if spir == 1:
-        if gas == 1:
-            for n in range(1,2):
-                for n in range(1,5):
-                    x=x+3
-                time.sleep(0.03)
-                for n in range(1,4):
-                    x=x+2
-                time.sleep(0.03)
-                for n in range(1,3):
-                    x=x+1
-            x=x+0
-            spir=0
-            gas = 0
-        if gas == 2:
-            for n in range(1,2):
-                for n in range(1,5):
-                    y=y+3
-                time.sleep(0.03)
-                for n in range(1,4):
-                    y=y+2
-                time.sleep(0.03)
-                for n in range(1,3):
-                    y=y+1
-            y=y+0
-            spir=0
-            gas = 0
-        if gas == 3:
-            for n in range(1,2):
-                for n in range(1,5):
-                    x=x-3
-                time.sleep(0.03)
-                for n in range(1,4):
-                    x=x-2
-                time.sleep(0.03)
-                for n in range(1,3):
-                    x=x-1
-            x=x+0
-            spir=0
-            gas = 0
-        if gas == 4:
-            for n in range(1,2):
-                for n in range(1,5):
-                    y=y-3
-                time.sleep(0.03)
-                for n in range(1,4):
-                    y=y-2
-                time.sleep(0.03)
-                for n in range(1,3):
-                    y=y-1
-            y=y+0
-            spir=0
-            gas = 0
-        
-    
+    if a1==True:
+        x1=x1+10
+    if x1==1300:
+        a1=False
+        b1=True
+        yl1=y1
+    if b1==True:
+        x1=x1-10
+    if x1==0:
+        a1=True 
+        b1=False
+        yl1=y1
     win.fill((0,0,0))
-    pygame.draw.rect(win, (0,0,200), (x,y,30,30))
-    pygame.display.update()
-
+    pygame.draw.rect(win,(255,0,0),(x1+45,yl1,10,50))
+    yl1=yl1+15
+    if yl1==700:
+        yl1=y1
+    sepiq(xx1,yy1)
+    if keys[pygame.K_a] and x>vel:
+        x=x-vel
+        left = True
+        right = False
+    elif keys[pygame.K_d] and x<1378-width-vel:
+        x=x+vel
+        left=False
+        right=True
+    elif keys[pygame.K_s]:
+        y=y+vel
+        down=True
+        left=False
+        right=False
+        up=False
+    elif keys[pygame.K_w]:
+        up=True
+        left=False
+        right=False
+        down=False
+        y=y-vel
+    else:
+        left = False
+        right = False
+        wc = 0
+    
+    sepiq(xx1,yy1)
+    redrawGameWindow()
 pygame.quit()
